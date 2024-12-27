9bd7a092eb75baf831f3b0cdcea847b098713c05
lagrandecode <seunogunmolufirst1@gmail.com>
2024-12-26 23:44:02 -0500

    there is need to add the status of the house

--- house/api/models.py
+++ house/api/models.py
@@ -7,6 +7,7 @@ class Grid(models.Model):
     image = models.ImageField(upload_to='images/')
     address = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=100,decimal_places=2)
+    # new = models.CharField(max_length=20)
 
     def __str__(self)-> str:
         return self.name
