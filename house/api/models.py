dd2e8df6548bb35f8b253a22d750aced26459d08
lagrandecode <seunogunmolufirst1@gmail.com>
2024-12-26 23:49:00 -0500

    "I changed the name of class to singular

--- house/api/models.py
+++ house/api/models.py
@@ -9,6 +9,10 @@ class Grid(models.Model):
     price = models.DecimalField(max_digits=100,decimal_places=2)
     # new = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name_plural = 'Grid'
+
     def __str__(self)-> str:
         return self.name
+
 
